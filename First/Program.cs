92cc4ecba4acc03970b75eb3e115dc64d35de691
Savin <savinn1989@mail.ru>
2022-03-02 23:15:15 +1000

    Задача 1

--- First/Program.cs
+++ First/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//домашнее задание 1 Задача 1
+Console.WriteLine ("Введите первое число:"); 
+string strValue=Console.ReadLine();
+int Value = int.Parse(strValue);
+
+Console.WriteLine ("Введите второе число:"); 
+string strValue1=Console.ReadLine();
+int Value1 = int.Parse(strValue1);
+
+if (Value<Value1)
+Console.WriteLine("Второе число больше первого"); 
+else
+Console.WriteLine("Первое число больше второго"); 
