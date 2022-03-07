e68afedd27a092955ce018c22a5ead8f6e5efd4c
Savin <savinn1989@mail.ru>
2022-03-07 15:01:02 +1000

    домашнее задание 2

---
+++ First_2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 1: Напишите программу, которая принимает на вход 
+// трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine ("Введите трехзначное число:"); 
+string strValue = Console.ReadLine();
+int Value = strValue.Length;
+ if (Value == 3)
+    {
+        int number = int.Parse(strValue);
+        int A = number/10;
+        int B = A%10;
+        Console.WriteLine ($"Вторая цифра: {B}"); 
+    }
+else
+    {
+    Console.WriteLine ("Ошибка: число не является трехзначным");    
+    }
+
---
+++ Third_2/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 3: Напишите программу, которая выводит 
+//третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.WriteLine ("Введите число:"); 
+string strValue = Console.ReadLine();
+int Value = strValue.Length;
+    if (Value < 3) Console.WriteLine ("Третья цифра отсутствует");
+    else
+        {
+        char thirdChar = strValue [2];
+        Console.WriteLine ($"{thirdChar}");    
+        }
