00f710d6190d5a6e807538eba2d13aae8166ec63
Savin <savinn1989@mail.ru>
2022-04-02 11:05:39 +1000

    Lesson_6

---
+++ Lesson_6/Task_1/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 1: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь
+
+
+int Promt(string msg)
+{
+    Console.WriteLine(msg);
+    string strValue = Console.ReadLine();
+    int Value = int.Parse(strValue);
+    return Value;
+}
+
+int[] CreateArray(int M)
+{
+    int[] array = new int[M];
+    for (int i = 0; i < M; i++)
+    {
+        array[i] = Promt ($"Введите число {i+1}:");
+    }
+    return array;
+}
+
+void PrintArr (int [] arr)
+{
+for (int i = 0; i < arr.Length-1; i++)
+    {
+        Console.Write (arr [i]+", ");
+    }
+    Console.WriteLine (arr [arr.Length-1]);
+}
+
+int CountPos (int [] arr)
+{
+    int count = 0;
+    for (int i = 0; i<arr.Length; i++)
+    {
+        if (arr [i] > 0) count++;
+    }
+    return count;
+}
+
+
+int M = Promt ("Какое количество чисел необходимоо ввести? =>");
+int [] numbers = CreateArray (M);
+Console.WriteLine ("Введенные числа:");
+PrintArr (numbers);
+Console.WriteLine ($"Количество положительных чисел = {CountPos(numbers)}");
---
+++ Lesson_6/Task_2/Program.cs
@@ -0,0 +1,34 @@
+﻿//Задача 2. Напишите программу, которая найдёт точку 
+//пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+//  x=(b2-b1)/(k1-k2) , y=k1(b2-b1)/(k1-k2)+b1
+
+
+double Promt(string msg)
+{
+    Console.Write(msg);
+    string strValue = Console.ReadLine();
+    double d1 = Convert.ToDouble(strValue);
+    double Value = double.Parse(strValue);
+    return Value;
+}
+
+double b1 = Promt("Значение b1 = ");
+double k1 = Promt("Значение k1 = ");
+double b2 = Promt("Значение b2 = ");
+double k2 = Promt("Значение k2 = ");
+
+Console.WriteLine($"b1={b1}, k1={k1}, b2={b2}, k2={k2}");
+
+if (k2 == k1 & b1 != b2) Console.WriteLine("Прямые параллельны - точки пересечения отсутствуют");
+else
+{
+    if (k2 == k1 & b1 == b2) Console.WriteLine("Прямые совпадают - бесконечное количество точек пересечения");
+    else
+    {
+        double x = (b2 - b1) / (k1 - k2);
+        double y = k1 * ((b2 - b1) / (k1 - k2)) + b1;
+        Console.WriteLine($"Координаты точки пересечения: x = {x}, y = {y}");
+    }
+}
