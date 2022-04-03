b2f798422d2bbc5c18f33eb6d15246eafe24be9c
Savin <savinn1989@mail.ru>
2022-04-03 18:55:36 +1000

    Lesson_7

---
+++ Lesson_7/Task2/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает позиции элемента 
+// в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+double[,] FillArray(int row, int column)
+{
+    double[,] array = new double[row, column];
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.NextDouble() * 100;
+        }
+    }
+    return array;
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]:0.00}   ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int Promt(string msg)
+{
+    Console.Write(msg);
+    string strValue = Console.ReadLine();
+    int Value = int.Parse(strValue);
+    return Value;
+}
+
+
+void FoundElement(double[,] array)
+{
+    int i = Promt("Введите номер строки: ") - 1;
+    int j = Promt("Введите номер столбца: ") - 1;
+    Console.WriteLine($"Значение искомого элемента: {array[i, j]:0.00}");
+}
+
+
+double[,] array = FillArray(4, 4);
+PrintArray(array);
+Console.WriteLine();
+FoundElement(array);
---
+++ Lesson_7/Task3/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задача 3: Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+int[,] FillArray(int row, int column)
+{
+    int[,] array = new int[row, column];
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.Next(0, 10);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int [,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}   ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void ColumnArithmeticMean (int[,] array)
+{
+    int sum = 0;
+    double arithMean = 0;
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            sum = sum + array[i, j];
+        }
+        arithMean = (double) sum / array.GetLength(0);
+        Console.Write($"{arithMean:0.00}  ");
+        sum = 0;
+        arithMean = 0;
+    }
+}
+
+
+
+
+int [,] array = FillArray(10, 10);
+PrintArray(array);
+Console.WriteLine("Среднее арифпетическое значений по столбцам =>");
+ColumnArithmeticMean(array);
