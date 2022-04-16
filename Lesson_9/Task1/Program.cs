8203452b97899ea26a96178f13c516b28035c1e1
Savin <savinn1989@mail.ru>
2022-04-16 11:24:27 +1000

    Lesson_9

---
+++ Lesson_9/Task1/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 1: Задайте значения M и N. Напишите программу, которая выведет все чётные натуральные числа 
+// в промежутке от M до N с помощью рекурсии.
+
+
+int Promt(string msg)
+{
+    Console.Write(msg);
+    string strValue = Console.ReadLine();
+    int Value = int.Parse(strValue);
+    return Value;
+}
+
+//для  m < n
+void PrintNaturalEvenNumberInc(int m, int n) 
+{
+    int firstEvenNum = 2;
+        if (n <= 0) Console.WriteLine("Четные натуральные числа в указанном диапазоне отсутствуют");
+        if (m % 2 != 0) m = m + 1;
+        if (m <= 0) m = firstEvenNum;
+        if (n < m) return;
+        Console.Write($"{m}\t");
+        PrintNaturalEvenNumberInc(m + 2, n);
+}
+// для m>n
+void PrintNaturalEvenNumberDec (int m, int n)
+{
+        int firstEvenNum = 2;
+        if (m < n) return;
+        if (n <= 0) n = firstEvenNum;
+        if (m <= 0) Console.WriteLine("Четные натуральные числа в указанном диапазоне отсутствуют");
+        if (m % 2 != 0) m = m - 1;
+        Console.Write($"{m}\t");
+        PrintNaturalEvenNumberDec(m - 2, n);
+}
+
+int m = Promt("Введите число M: ");
+int n = Promt("Введите число N: ");
+Console.WriteLine($"Натуральные четные числа от {m} до {n}: ");
+if (m<n) PrintNaturalEvenNumberInc (m, n);
+else PrintNaturalEvenNumberDec (m, n);
+
---
+++ Lesson_9/Task2/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 2: Задайте значения M и N. Напишите программу, которая найдёт сумму 
+// натуральных элементов в промежутке от M до N используя рекурсию.
+
+
+int Promt(string msg)
+{
+    Console.Write(msg);
+    string strValue = Console.ReadLine();
+    int Value = int.Parse(strValue);
+    return Value;
+}
+
+//для  m < n
+int PrintNaturalNumberSumInc (int m, int n) 
+{
+    int firstNutNum = 1;
+    int sum = 0;
+        if (n <= 0) Console.WriteLine("Натуральные числа в указанном диапазоне отсутствуют");
+        if (m <= 0) m = firstNutNum;
+        if (m > n) return 0;
+        sum = sum + m + PrintNaturalNumberSumInc (m + 1, n);
+    return sum;
+}
+
+// для m>n
+int PrintNaturalNumberSumDec (int m, int n)
+{
+        int firstNutNum = 1;
+        int sum = 0;
+        if (m < n) return 0;
+        if (n <= 0) n = firstNutNum;
+        if (m <= 0) Console.WriteLine("Натуральные числа в указанном диапазоне отсутствуют");
+        sum = sum + m + PrintNaturalNumberSumDec (m - 1, n);
+    return sum;
+}
+
+int m = Promt("Введите число M: ");
+int n = Promt("Введите число N: ");
+Console.WriteLine($"Сумма натуральных чисел от {m} до {n}: ");
+if (m<n) Console.WriteLine ($"{PrintNaturalNumberSumInc (m, n)}");
+else Console.WriteLine ($"{PrintNaturalNumberSumDec (m, n)}");
