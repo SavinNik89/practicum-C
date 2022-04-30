08af8e0e46b11fec396ffef6340c1a87331f8dfe
Savin <savinn1989@mail.ru>
2022-04-30 10:31:37 +1000

    Lesson_11

---
+++ Lesson_11/Task1_Power_2/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 1: Определите, является ли число степенью двойки:
+//N = 16 -> "Является степень двойки"
+//N = 12 -> “Не является степенью двойки”
+
+
+void Power2(int N)
+{
+    double count = 0;
+    int pow = 0;
+    if (N == 1) 
+    {
+        count = 0;
+        pow = 0;
+    }
+    else
+    {
+        if (N > 1)
+        {
+            while (N > 1)
+            {
+                count = count + (N % 2);
+                N = N / 2;
+                pow++;
+            }
+        }
+    }
+   if (count == 0) Console.WriteLine($"Число является {pow} степенью двойки");
+   else Console.WriteLine("Число не является степенью двойки");
+}
+
+Console.WriteLine("Введите число для проверки:");
+int N = Convert.ToInt32(Console.ReadLine());
+Power2 (N);
---
+++ Lesson_11/Task2_Check_SimpleNumber/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 2: Проверка на простое число:
+// N = 13 -> "Это простое число"
+// N = 12 -> “Это не простое число”
+
+int[] FillArray(int N)
+{
+    int[] array = new int[N];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = i + 1;
+    }
+    return array;
+}
+
+void CheckNumber(int N)
+{
+    int[] array = FillArray(N);
+    for (int i = 1; i < array.Length - 1; i++)
+    {
+        if (N % array[i] == 0)
+        {
+            Console.WriteLine("Число не является простым");
+            return;
+        }
+    }
+    Console.WriteLine("Число является простым");
+}
+
+
+Console.WriteLine("Введите число для проверки:");
+int N = Convert.ToInt32(Console.ReadLine());
+CheckNumber (N);
